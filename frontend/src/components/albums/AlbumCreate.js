6e54986fafee73ea86f9c18713a466c1fa2654b7
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2022-10-15 15:08:30 +0530

    Form: - Create Album Form added. - Redux Devtools added.

--- frontend/src/components/albums/AlbumCreate.js
+++ frontend/src/components/albums/AlbumCreate.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Field, reduxForm } from "redux-form";
 
-const AlbumCreate = () => {
-    return ( <div><h1>Album Create</h1></div> );
+class AlbumCreate extends React.Component {
+  renderTitleInput(formProps) {
+    return (
+      <div className="field">
+        <label>{formProps.title}</label>
+        <input {...formProps.input} />
+      </div>
+    );
+  }
+
+  onFormSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+        className="ui form"
+        onSubmit={this.props.handleSubmit(this.onFormSubmit)}
+      >
+        <Field
+          name="title"
+          component={this.renderTitleInput}
+          title="Album Title: "
+        />
+        <button className="ui button primary">Create Album</button>
+      </form>
+    );
+  }
 }
- 
-export default AlbumCreate;
+
+export default reduxForm({
+  form: "albumCreate",
+})(AlbumCreate);
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { reducer as reduxFormReducer} from 'redux-form';
 import oAuthReducer from "./oAuthReducer";
 
 export default combineReducers({
   oAuth: oAuthReducer,
+  form: reduxFormReducer,
 });
