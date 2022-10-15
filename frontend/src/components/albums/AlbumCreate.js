ef28af1e9d5f69d711b03363c218f4b91bcd9e91
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2022-10-15 18:12:34 +0530

    Form validation: - Create Almbum form Validation added. - Submit button also visible when form valid. - Error message shown if form invalid. - Field with wrong value is highlighted.

--- frontend/src/components/albums/AlbumCreate.js
+++ frontend/src/components/albums/AlbumCreate.js
@@ -2,36 +2,55 @@ import React from 'react';
 import { Field, reduxForm } from "redux-form";
 
 class AlbumCreate extends React.Component {
-  renderTitleInput(formProps) {
-    return (
-      <div className="field">
-        <label>{formProps.title}</label>
-        <input {...formProps.input} />
-      </div>
-    );
+  renderInputError(meta) {
+    if (meta.touched && meta.error) {
+      return <div className="ui error message">{meta.error}</div>;
+    }
   }
 
+  renderTitleInput = (formProps) => {
+    return (
+      // CSS to highlight field's with error.
+      <div className={`field ${formProps.meta.touched && formProps.meta.error ? "error" : ""}`}>
+        <label>{formProps.title}</label>
+        <input {...formProps.input} />
+        {/* Show error only when input is touched and still error persists. */}
+        {this.renderInputError(formProps.meta)}
+      </div>
+    );
+  };
+
   onFormSubmit(formValues) {
+    // todo: only console logginh right now. Not submititing.
     console.log(formValues);
   }
 
   render() {
     return (
-      <form
-        className="ui form"
-        onSubmit={this.props.handleSubmit(this.onFormSubmit)}
-      >
-        <Field
-          name="title"
-          component={this.renderTitleInput}
-          title="Album Title: "
-        />
-        <button className="ui button primary">Create Album</button>
+      <form className="ui form error" onSubmit={this.props.handleSubmit(this.onFormSubmit)}>
+        <Field name="title" component={this.renderTitleInput} title="Album Title: " />
+        <button className={`ui button ${this.props.valid ? "primary" : "disable"}`}>Create Album</button>
       </form>
     );
   }
 }
 
+// Errors after validation passed to meta property of that form-input's formProps.
+const validateFormValues = (formValues) => {
+  const errors = {};
+
+  // If form is empty, then can't create Album.
+  if (!formValues.title) {
+    errors.title = "Empty Album title not allowed!";
+  }
+
+  // can append more errors for other cases:
+
+  // finally return errors. If errors is empty object, then no redux form error.
+  return errors;
+};
+
 export default reduxForm({
   form: "albumCreate",
+  validate: validateFormValues,
 })(AlbumCreate);
