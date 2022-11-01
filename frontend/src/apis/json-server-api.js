24882bd1a10daa7663ccd9d3b349ade8dd678dd7
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2022-11-01 17:19:02 +0530

    removed port from json-server url

--- frontend/src/apis/json-server-api.js
+++ frontend/src/apis/json-server-api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // JSON-Server hosted on port 3003, to receive album HTTP requests.
 export default axios.create({
-  baseURL: "https://react-albumzz-backend.herokuapp.com/:3003",
+  baseURL: "https://react-albumzz-backend.herokuapp.com/",
 });
